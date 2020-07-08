ac53ea38f0fd5c7aebc58e6c0d968e25c0a2708d
aniket00100 <aniketkale.ak@gmail.com>
2020-07-08 08:57:45 +0530

    immortal snake build

---
+++ build/precache-manifest.c574f5435f50c3c685c7a811f31e6b59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbcdddc5108fdb0cc9181ce8f6b5a0f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b2bb9814463c5a5c76c",
+    "url": "/static/css/main.30aab9d3.chunk.css"
+  },
+  {
+    "revision": "fb3d0fdd9edf305f8020",
+    "url": "/static/js/2.6c19d4cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c19d4cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2bb9814463c5a5c76c",
+    "url": "/static/js/main.357f7bb9.chunk.js"
+  },
+  {
+    "revision": "57038c7ad65e3c9693a3",
+    "url": "/static/js/runtime-main.fdb84dde.js"
+  }
+]);
